425e6120f128f20f8eedf2df443941d94250a2c1
kserks <bakakasin@gmail.com>
2022-09-10 00:50:39 +0700

    latest

--- src/plugins/mc-get/block-controller.js
+++ src/plugins/mc-get/block-controller.js
@@ -1,54 +1,122 @@
-export default {
-    /*
-    isMinecraft (){
-        if(!vnjs.store.MINECRAFT?.BLOCK){
-            vnjs.emit('vnjson.error', `Плагин работает только внутри Minecraft`)
-            return false
+/**
+ * mc-get-block
+ */
+
+export function mcGetBlockCommon(args) {
+    const props = ["color", "variant", "axis", "facing"];
+    const block = vnjs.store.MINECRAFT.BLOCK.data;
+
+    let idExist = false;
+    for (let key in args) {
+        /**
+         * Обрабатываю ID
+         */
+        if (!props.includes(key) && key !== "default" && block.id === key) {
+            idExist = true;
+            vnjs.exec(args[key]);
         }
-        return true
-    },*/
-    "mc-get-block" (args){
-        const block = vnjs.store.MINECRAFT.BLOCK;
-        let flag = false
-        for(let key in args){
-            if (key === block.data.id) {
-                flag = true
-                vnjs.exec(args[key]);
-            }
-        }
-        if(!flag){
-            if(args.default){
-                vnjs.exec(args.default);
-           }
-        }
-    },
-    handlerProp (args, prop){
-        const block = vnjs.store.MINECRAFT.BLOCK;
-        let flag = false
-        for(let key in args){
-            const [ id, param ] = key.split('.');
-            let blockParam = null;
-            for (let blockKey in block.data.state) {
-                if (blockKey.includes(`name=${prop}`)) {
-                    blockParam = block.data.state[blockKey].toLowerCase();
+        /**
+         * Обрабатвываю свойства ["color", "variant", "axis", "facing"];
+         */
+        if (props.includes(key)) {
+            /**
+             * Пробегаю по свойтвам блока
+             */
+            for (let blockKey in block.state) {
+                if (blockKey.includes(`name=${key}`)) {
+                    let blockParam = block.state[blockKey];
+                    /**
+                     * Передаю value блока в переменную заданную пользователем color: color123
+                     */
+                    vnjs.state.data[args[key]] = blockParam;
                 }
             }
-            if (id === block.data.id && blockParam === param) {
-                flag = true
-                vnjs.exec(args[key]);
-            }
-        }
-        if(!flag){
-            if(args.default){
-                vnjs.exec(args.default);
-           }
-        }
-    },
-    "mc-get-block-nbt" (args) {
-        const block = vnjs.store.MINECRAFT.BLOCK;
-        for(let key in args){
-            let value = block.data.nbt[key];
-            vnjs.state.data[ args[key] ] = value;
         }
     }
+
+    if (!idExist && args.default) {
+        vnjs.exec(args.default);
+    }
+}
+
+/**
+ * mc-get-block-nbt
+ */
+
+export function mcGetBlockNBT(args) {
+    const block = vnjs.store.MINECRAFT.BLOCK;
+    for (let key in args) {
+        let value = block.data.nbt[key];
+        vnjs.state.data[args[key]] = value;
+    }
+}
+
+/**
+ * mc-get-store-slot
+ */
+
+export function mcGetStoreSlot(args) {
+    const { slot } = args;
+    const notIDProp = ["count", "damage", "slot"];
+    const block = vnjs.store.MINECRAFT.BLOCK;
+    const items = block.data.nbt.Items;
+
+    const _item = items.find((item) => item.Slot === slot);
+    if (!_item) {
+        if (args.default) {
+            vnjs.exec(args.default);
+        }
+        return;
+    }
+    vnjs.state.data[args.count] = _item.Count;
+    vnjs.state.data[args.damage] = _item.Damage;
+    let idExist = false;
+    for (let key in args) {
+        if (key === _item.id) {
+            idExist = true;
+            vnjs.exec(args[key]);
+        }
+    }
+
+    if (!idExist && args.default) {
+        vnjs.exec(args.default);
+    }
+}
+
+/**
+ * mc-get-store-total
+ */
+export function mcGetStoreTotal(args) {
+    const block = vnjs.store.MINECRAFT.BLOCK;
+    const items = block.data.nbt.Items;
+    const _items = items.filter((item) => {
+        if (args.damage) {
+            if (item.id === args.id && item.Damage === args.damage) {
+                return true;
+            }
+        } else {
+            if (item.id === args.id) {
+                return true;
+            }
+        }
+    });
+    /**
+     * formula
+     */
+    let sum = 0;
+    if (args.formula === "SumRateCount") {
+        args.rates.forEach((item) => {
+            let key = Object.keys(item)[0];
+
+            if (args.damage === +key) {
+                _items.forEach((_i) => {
+                    sum += Number(item[args.damage]) * _i.Count;
+                });
+            }
+        });
+        vnjs.state.data[args.result] = sum;
+        console.log(sum);
+    }
+
+    console.log(_items);
 }
--- src/plugins/mc-get/index.js
+++ src/plugins/mc-get/index.js
@@ -1,9 +1,7 @@
-import mcGet from './mc-get.js';
-import blockController from "./block-controller";
+import mcGet from "./mc-get.js";
+import * as blockController from "./block-controller";
 
-
-
-export default function () {};
+export default function () {}
 /**
  * получаем блок
  */
@@ -11,64 +9,21 @@ vnjs.on("mc-get", mcGet);
 /**
  * сравнение по ID
  */
-vnjs.on("mc-get-block", (args) => blockController["mc-get-block"](args));
-/**
- * сравнение по цвету
- */
-vnjs.on("mc-get-block-color", (args) => blockController.handlerProp(args, "color") );
-
-/**
- * сравнение по типу
- */
-vnjs.on("mc-get-block-variant", (args) => blockController.handlerProp(args, "variant") );
-
-/**
- * сравнение по направлению раположения
- */
-vnjs.on("mc-get-block-axis", (args) => blockController.handlerProp(args, "axis") );
-
-/**
- * куда смотрит предмет
- */
-vnjs.on("mc-get-block-facing", (args) => blockController.handlerProp(args, "facing") );
+vnjs.on("mc-get-block", blockController.mcGetBlockCommon);
 
 /**
  * nbt
  */
 
-vnjs.on('mc-get-block-nbt', (args) => blockController["mc-get-block-nbt"](args) )
+vnjs.on("mc-get-block-nbt", blockController.mcGetBlockNBT);
 
 /**
  * get chest
  */
-const notIDProp = ['count', 'damage', 'slot'];
-vnjs.on('mc-get-store-slot', (args) => {
-    const { slot } = args;
-    const block = vnjs.store.MINECRAFT.BLOCK;
-    const items = block.data.nbt.Items;
-   
-    const _item = items.find( (item) => item.Slot === slot);
-    if(!_item){
-        if(args.default){
-            vnjs.exec(args.default)
-        }
-        return;
-    }
-    vnjs.state.data[args.count] = _item.Count;
-    vnjs.state.data[args.damage] = _item.Damage;
-    let idExist = false;
-    for(let key in args){
-        if(key===_item.id){
-            idExist = true;
-            vnjs.exec(args[key]);
-        }
 
-    }
+vnjs.on("mc-get-store-slot", blockController.mcGetStoreSlot);
 
-    if(!idExist && args.default){
-        vnjs.exec(args.default)
-    }
-
-
-    console.log(block.data.nbt)
-})
+/**
+ * get chest total items
+ */
+vnjs.on("mc-get-store-total", blockController.mcGetStoreTotal);
